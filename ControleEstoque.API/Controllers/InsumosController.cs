ba171e6d501455e5a35be580009ee46716230f19
Wrsil <Wrsil@WILLIAM>
2026-01-28 22:15:03 -0300

    Adicionar arquivos de projeto.

---
+++ ControleEstoque.API/Controllers/InsumosController.cs
@@ -0,0 +1,21 @@
+﻿using ControleEstoque.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleEstoque.API.Controllers
+{
+    [ApiController]
+
+    public class InsumosController : Controller
+    {
+        public async Task<List<Insumos>> ListarInsumosAsync()
+        {
+            var insumos = new List<Insumos>();
+            return insumos;
+        }
+
+        public async Task<List<Insumos>> BuscarInsumoPorIdAsync(int id)
+        {
+            return new List<Insumos>(); 
+        }
+    }
+}
---
+++ ControleEstoque.API/Entities/Insumos.cs
@@ -0,0 +1,8 @@
+﻿namespace ControleEstoque.API.Entities
+{
+    public class Insumos
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
