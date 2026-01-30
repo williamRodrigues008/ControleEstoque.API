ce11ac90a0a270b5def1fdcfb324c05a67df46b7
williamRodrigues008 <Wrsilva008@gmail.com>
2026-01-29 21:09:53 -0300

    Instalação e utilização de pacotes entityframework.

--- ControleEstoque.API/Program.cs
+++ ControleEstoque.API/Program.cs
@@ -1,4 +1,14 @@
+using ControleEstoque.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BdContexto>(
+    options => options
+        .UseSqlServer(builder.Configuration.GetConnectionString("Default")
+    )
+);
 
 // Add services to the container.
 
