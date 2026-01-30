936b07bbc5677e4cdb6bbf9915e0c1c7c2004bcb
williamRodrigues008 <Wrsilva008@gmail.com>
2026-01-29 21:06:56 -0300

    Criação da interface de insumos

---
+++ ControleEstoque.API/Interfaces/IInsumos.cs
@@ -0,0 +1,12 @@
+﻿using ControleEstoque.API.Entities;
+
+namespace ControleEstoque.API.Interfaces
+{
+    public interface IInsumos
+    {
+        public Task<IEnumerable<Insumo>> BuscarInsumos();
+        public Task<IEnumerable<Insumo>> BuscarInsumoPorId(int id);
+        public Task<IEnumerable<Insumo>> AdicionarInsumo(Insumo insumo);
+
+    }
+}
