d7c58181ed8ea0003de598626cfabd0e63db6b88
williamRodrigues008 <Wrsilva008@gmail.com>
2026-01-29 21:05:03 -0300

    Criação do contexto de banco de dados

---
+++ ControleEstoque.API/Data/BdContexto.cs
@@ -0,0 +1,12 @@
+﻿using ControleEstoque.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleEstoque.API.Data
+{
+    public class BdContexto : DbContext
+    {
+        public BdContexto(DbContextOptions<DbContext> options) : base(options) { }
+
+        DbSet<Insumo> Insumos { get; set; }
+    }
+}
