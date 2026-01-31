e436b34184d94e2f9f87ff5ff5beb3d1957b5f0d
williamRodrigues008 <Wrsilva008@gmail.com>
2026-01-31 16:56:04 -0300

    Criação de serviço das movimentações e adição de propriedades a classe de Movimentação

--- ControleEstoque.API/Entities/MovimentacaoInsumos.cs
+++ ControleEstoque.API/Entities/MovimentacaoInsumos.cs
@@ -2,5 +2,11 @@
 {
     public class MovimentacaoInsumos
     {
+        public int Id { get; set; }
+        public string Local {  get; set; } = string.Empty;
+        public DateTime DataMovimentacao { get; set; }
+        public ItensMovimentados ItensMovimentados { get; set; } = new();
+        public string Solicitante { get; set; } = string.Empty;
+        public string Responsavel { get; set; } = string.Empty;
     }
 }
---
+++ ControleEstoque.API/Services/MovimentacaoService.cs
@@ -0,0 +1,50 @@
+﻿using ControleEstoque.API.Data;
+using ControleEstoque.API.Entities;
+using ControleEstoque.API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleEstoque.API.Services
+{
+    public class MovimentacaoService : IMovimentacaoInsumos
+    {
+        private readonly BdContexto _bdContexto;
+
+        public MovimentacaoService(BdContexto bdContexto)
+        {
+            _bdContexto = bdContexto;
+        }
+
+        public bool AdicionarMovimentacaoInsumo(MovimentacaoInsumos movimentacao)
+        {
+            if (movimentacao is not null)
+                _bdContexto.MovimentacaoInsumos.Add(movimentacao);
+              
+            if (_bdContexto.SaveChanges() > 0)
+                    return true;
+            return false;
+        }
+
+        public ItensMovimentados BuscarItemPorId(int id) => _bdContexto.ItensMovimentados.SingleOrDefault(x => x.IdMovimentacao == id)!;
+
+        public MovimentacaoInsumos BuscarMovimentacaoPorId(int id) => _bdContexto.MovimentacaoInsumos.SingleOrDefault(x => x.Id == id)!;
+
+        public MovimentacaoInsumos EditarMovimentacao(int id) => BuscarMovimentacaoPorId(id);
+        public bool ExcluirMovimentacao(int id)
+        {
+            _bdContexto.MovimentacaoInsumos.Where(x => x.Id == id).ExecuteDelete();
+            if (_bdContexto.SaveChanges() > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public List<ItensMovimentados> ListarItensMovimentados(int idMovimentacao) => _bdContexto.ItensMovimentados.Where(x => x.IdMovimentacao == idMovimentacao).ToList();
+
+        public List<MovimentacaoInsumos> ListarMovimentacoes()
+        {
+            return _bdContexto.MovimentacaoInsumos
+                .Take(20)
+                .ToList();
+        }
+    }
+}
