272dbf6686db4446f92b783a59ece614121910e1
williamRodrigues008 <Wrsilva008@gmail.com>
2026-01-31 16:54:03 -0300

    Criação de interface de movimentação de insumos

---
+++ ControleEstoque.API/Interfaces/IMovimentacaoInsumos.cs
@@ -0,0 +1,15 @@
+﻿using ControleEstoque.API.Entities;
+
+namespace ControleEstoque.API.Interfaces
+{
+    public interface IMovimentacaoInsumos
+    {
+        List<MovimentacaoInsumos> ListarMovimentacoes();
+        MovimentacaoInsumos BuscarMovimentacaoPorId(int id);
+        List<ItensMovimentados> ListarItensMovimentados(int idMovimentacao);
+        ItensMovimentados BuscarItemPorId(int id);
+        bool AdicionarMovimentacaoInsumo(MovimentacaoInsumos movimentacao);
+        bool ExcluirMovimentacao(int id);
+        MovimentacaoInsumos EditarMovimentacao(int id);
+    }
+}
