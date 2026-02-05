98116df9acbb3588b08b4f6150928c08bdc2d9cb
williamRodrigues008 <Wrsilva008@gmail.com>
2026-02-04 23:31:57 -0300

    Rename de dbset do contexto do banco

--- ControleEstoque.API/Data/BdContexto.cs
+++ ControleEstoque.API/Data/BdContexto.cs
@@ -6,9 +6,8 @@ namespace ControleEstoque.API.Data
     public class BdContexto : DbContext
     {
         public BdContexto(DbContextOptions<BdContexto> options) : base(options) { }
-
         public DbSet<Insumo> Insumos { get; set; }
-        public DbSet<Movimentacao> MovimentacaoInsumos { get; set; }
+        public DbSet<Movimentacao> MovimentacaoInsumo { get; set; }
         public DbSet<ItemMovimentado> ItensMovimentados { get; set; }
         public DbSet<Quimico> Quimico { get; set; }
     }
