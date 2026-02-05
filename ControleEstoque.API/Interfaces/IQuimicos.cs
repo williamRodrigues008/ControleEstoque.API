3ab5c0bcec77832f27df7fdc7576713b4b6b9167
williamRodrigues008 <Wrsilva008@gmail.com>
2026-02-04 23:33:25 -0300

    Adição de novo contrato da interface de quimicos

--- ControleEstoque.API/Interfaces/IQuimicos.cs
+++ ControleEstoque.API/Interfaces/IQuimicos.cs
@@ -5,6 +5,7 @@ namespace ControleEstoque.API.Interfaces
     public interface IQuimicos
     {
         List<Quimico> ListarQuimicos();
+        Quimico BuscarQuimicoPorId(int id);
         bool AdicionarQuimico(Quimico quimico);
         bool EditarQuimico(Quimico quimico);
         bool ExcluirQuimico(int id);
