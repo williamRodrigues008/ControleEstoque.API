f33882eef986b05a0e710bd89e56b6d2266dffdf
williamRodrigues008 <Wrsilva008@gmail.com>
2026-02-08 22:56:45 -0300

    Edição de titulo, ajuste para standalone, e chamada de componente no app root

--- ControleEstoque.APP/src/app/app.component.ts
+++ ControleEstoque.APP/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+import { InsumoComponent } from './components/insumo/insumo.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, InsumoComponent]
 })
 export class AppComponent {
-  title = 'ControleEstoque.APP';
+  title = 'Controle Estoque';
 }
--- ControleEstoque.APP/src/main.ts
+++ ControleEstoque.APP/src/main.ts
@@ -1,8 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
-})
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient()
+  ]
+}).catch(err => console.error(err));
