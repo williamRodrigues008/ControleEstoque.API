15dae42317375f0e39e2a2c51838d4da0f0fd124
williamRodrigues008 <Wrsilva008@gmail.com>
2026-02-11 21:26:10 -0300

    adição do providerrouter no main.ts e importação das rotas.

--- ControleEstoque.APP/src/main.ts
+++ ControleEstoque.APP/src/main.ts
@@ -1,9 +1,14 @@
+/// <reference types="@angular/localize" />
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app-routing.module'
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient()
+    provideHttpClient(),
+    provideRouter(routes)
   ]
 }).catch(err => console.error(err));
