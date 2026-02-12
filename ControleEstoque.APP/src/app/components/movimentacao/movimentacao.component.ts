f1ada35d2799442e0e1893953be683d29e2f1498
williamRodrigues008 <Wrsilva008@gmail.com>
2026-02-11 21:55:34 -0300

    Criação do componente de movimentações

---
+++ ControleEstoque.APP/src/app/components/movimentacao/movimentacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movimentacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './movimentacao.component.html',
+  styleUrl: './movimentacao.component.css'
+})
+export class MovimentacaoComponent {
+
+}
