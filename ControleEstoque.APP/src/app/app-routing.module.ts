4e83720dc613270d22f2d57f4dd915f270bd39fc
williamRodrigues008 <Wrsilva008@gmail.com>
2026-02-11 21:58:49 -0300

    Importação do componente de movimentação para rotas e retirada de duplicidade dos links

--- ControleEstoque.APP/src/app/app-routing.module.ts
+++ ControleEstoque.APP/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InsumoComponent } from './components/insumo/insumo.component';
 import { NotFoundError } from 'rxjs';
+import { MovimentacaoComponent } from './components/movimentacao/movimentacao.component';
 
 export const routes: Routes = [
-  { path: 'home', component: InsumoComponent },
+  { path: 'home', component: MovimentacaoComponent },
   { path: "insumos", component: InsumoComponent},
   { path: "quimicos", component: NotFoundError }
 ];
