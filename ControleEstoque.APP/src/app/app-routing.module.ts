732647baf5d4188ab25a0ba0628fe44b6419a2f8
williamRodrigues008 <Wrsilva008@gmail.com>
2026-02-14 23:37:50 -0300

    adição de rota para adicionarmovimentação

--- ControleEstoque.APP/src/app/app-routing.module.ts
+++ ControleEstoque.APP/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { InsumoComponent } from './components/insumo/insumo.component';
 import { NotFoundError } from 'rxjs';
 import { MovimentacaoComponent } from './components/movimentacao/movimentacao.component';
+import { AdicionarMovimentacaoComponent } from './components/modalcomponents/adicionar-movimentacao/adicionar-movimentacao.component';
 
 export const routes: Routes = [
   { path: 'home', component: MovimentacaoComponent },
   { path: "insumos", component: InsumoComponent},
-  { path: "quimicos", component: NotFoundError }
+  { path: "quimicos", component: NotFoundError },
+  { path: "novaMovimentacao", component: AdicionarMovimentacaoComponent }
 ];
 
 @NgModule({
