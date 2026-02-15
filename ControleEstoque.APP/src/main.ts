1de3945f0ba7a36491494e5e6d0df9c5ec22f259
williamRodrigues008 <Wrsilva008@gmail.com>
2026-02-14 23:39:41 -0300

    Ajuste de personalização de botão para adicionar movimentação

--- ControleEstoque.APP/src/main.ts
+++ ControleEstoque.APP/src/main.ts
@@ -4,11 +4,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app-routing.module'
+import { routes } from './app/app-routing.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes), provideAnimationsAsync()
   ]
 }).catch(err => console.error(err));
