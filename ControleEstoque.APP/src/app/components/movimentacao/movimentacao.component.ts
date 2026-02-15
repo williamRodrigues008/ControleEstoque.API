403b5c279c37d2c4428d8189d55293c22fe70433
williamRodrigues008 <Wrsilva008@gmail.com>
2026-02-14 23:33:41 -0300

    criação da tabelas de movimentações

--- ControleEstoque.APP/src/app/components/movimentacao/movimentacao.component.ts
+++ ControleEstoque.APP/src/app/components/movimentacao/movimentacao.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { movimentacao } from '../../models/movimentacao';
+import { MovimentacaoService } from '../../core/services/movimentacao.service';
 
 @Component({
   selector: 'app-movimentacao',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './movimentacao.component.html',
   styleUrl: './movimentacao.component.css'
 })
 export class MovimentacaoComponent {
+movimentacoes$!: Observable<movimentacao[]>;
 
+  constructor(private movimentacaoService: MovimentacaoService) {
+    this.getMovimentacoes();
+  }
+
+  getMovimentacoes(){
+    this.movimentacoes$ = this.movimentacaoService.getMovimentacao();
+  }
 }
