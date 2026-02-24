90556a4d186960157360552ce39cca2b6df9ba73
williamRodrigues008 <Wrsilva008@gmail.com>
2026-02-23 22:40:42 -0300

    adição da propriedade de unidade e movimentação

--- ControleEstoque.API/Entities/ItemMovimentado.cs
+++ ControleEstoque.API/Entities/ItemMovimentado.cs
@@ -1,11 +1,16 @@
-﻿namespace ControleEstoque.API.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ControleEstoque.API.Entities
 {
     public class ItemMovimentado
     {
         public int Id { get; set; }
-        public int IdMovimentacao { get; set; }
+        public int MovimentacaoId { get; set; }
+        [NotMapped]
+        public Movimentacao? Movimentacao { get; set; }
         public string Nome { get; set; } = string.Empty;
         public int Quantidade { get; set; }
+        public string Unidade { get; set; } = string.Empty;
         public bool ProdutoQuimico { get; set; }
         public string? Tipo{ get; set; }
     }
