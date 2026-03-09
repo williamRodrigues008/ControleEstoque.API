9d53a5c583a4a59353b6f868567e00f5d0fd661c
williamRodrigues008 <Wrsilva008@gmail.com>
2026-03-09 14:55:47 -0300

    Criação das interfaces Local e Unidade

---
+++ ControleEstoque.API/Interfaces/ILocal.cs
@@ -0,0 +1,13 @@
+﻿using ControleEstoque.API.Entities;
+
+namespace ControleEstoque.API.Interfaces
+{
+    public interface ILocal
+    {
+        List<Local> ListarLocais();
+        bool AdicionarLocal(Local local);
+        bool ExcluirLocal(int id);
+        Local BucarLocalPorId(int id);
+        bool AtualizarLocal(Local local);
+    }
+}
---
+++ ControleEstoque.API/Interfaces/IUnidade.cs
@@ -0,0 +1,13 @@
+﻿using ControleEstoque.API.Entities;
+
+namespace ControleEstoque.API.Interfaces
+{
+    public interface IUnidade
+    {
+        List<Unidade> ListarUnidade();
+        bool AdicionarUnidade(Unidade unidade);
+        bool ExcluirUnidade(int id);
+        Unidade BucarUnidadePorId(int id);
+        bool AtualizarUnidade(Unidade Unidade);
+    }
+}
