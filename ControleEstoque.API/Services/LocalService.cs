5214cbefb0c9865e2c0c45684cce213792f5100b
williamRodrigues008 <Wrsilva008@gmail.com>
2026-03-09 14:57:11 -0300

    Criação dos serviços de Local e Unidade

---
+++ ControleEstoque.API/Services/LocalService.cs
@@ -0,0 +1,48 @@
+﻿using ControleEstoque.API.Data;
+using ControleEstoque.API.Entities;
+using ControleEstoque.API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleEstoque.API.Services
+{
+    public class LocalService : ILocal
+    {
+        private readonly BdContexto _context;
+
+        public LocalService(BdContexto context)
+        {
+            _context = context;
+        }
+
+        public bool AdicionarLocal(Local local)
+        {
+            if (local is not null)
+                _context.Local.Add(local);
+            return _context.SaveChanges() > 0;
+        }
+
+        public bool AtualizarLocal(Local local)
+        {
+            _context.Local.Where(x => x.Id == local.Id).ExecuteUpdate(l => l
+            .SetProperty(p => p.Nome, local.Nome)
+            .SetProperty(p => p.Tipo, local.Tipo)
+            );
+            return _context.SaveChanges() > 0;
+        }
+
+        public Local BucarLocalPorId(int id)
+        {
+            return _context.Local.SingleOrDefault(x => x.Id == id)!;
+        }
+
+        public bool ExcluirLocal(int id)
+        {
+            return _context.Local.Where(l => l.Id == id).ExecuteDelete() > 0;
+        }
+
+        public List<Local> ListarLocais()
+        {
+            return _context.Local.ToList();
+        }
+    }
+}
---
+++ ControleEstoque.API/Services/UnidadeService.cs
@@ -0,0 +1,50 @@
+﻿using ControleEstoque.API.Data;
+using ControleEstoque.API.Entities;
+using ControleEstoque.API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleEstoque.API.Services
+{
+    public class UnidadeService : IUnidade
+    {
+        private readonly BdContexto _contexto;
+
+        public UnidadeService(BdContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public bool AdicionarUnidade(Unidade unidade)
+        {
+            if (unidade is not null)
+                _contexto.Unidade.Add(unidade);
+            return _contexto.SaveChanges() > 0;
+        }
+
+        public bool AtualizarUnidade(Unidade unidade)
+        {
+            if (unidade is not null)
+                _contexto.Unidade.Where(u => u.Id == unidade.Id)
+                    .ExecuteUpdate(uni => uni
+                    .SetProperty(i => i.Nome, unidade.Nome));
+
+            return _contexto.SaveChanges() > 0;
+        }
+
+        public Unidade BucarUnidadePorId(int id)
+        {
+            return _contexto.Unidade.SingleOrDefault(u => u.Id == id)!;
+        }
+
+        public bool ExcluirUnidade(int id)
+        {
+           return _contexto.Unidade.Where(u => u.Id == id).ExecuteDelete() > 0;
+            
+        }
+
+        public List<Unidade> ListarUnidade()
+        {
+            return _contexto.Unidade.ToList();
+        }
+    }
+}
