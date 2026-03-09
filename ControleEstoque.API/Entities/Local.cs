d0193f44727207943913e1b9e6e2435b3db960d1
williamRodrigues008 <Wrsilva008@gmail.com>
2026-03-09 14:55:25 -0300

    Criação das entidades Local e Unidade

---
+++ ControleEstoque.API/Entities/Local.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleEstoque.API.Entities
+{
+    public class Local
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Tipo { get; set; } = string.Empty;
+    }
+}
---
+++ ControleEstoque.API/Entities/Unidade.cs
@@ -0,0 +1,8 @@
+﻿namespace ControleEstoque.API.Entities
+{
+    public class Unidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+    }
+}
